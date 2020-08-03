6172d062c5423828c84076527efcf912ecee4c7d
Carlos_Gutierrez <carlosgutierrez7233@gmail.com>
2020-08-03 19:18:26 +0200

    Initial code

---
+++ src/main/java/com/cdsacademy/javapracticeone/JavaPracticeOneApplication.java
@@ -0,0 +1,13 @@
+package com.cdsacademy.javapracticeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPracticeOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdsacademy/javapracticeone/JavaPracticeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdsacademy.javapracticeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
